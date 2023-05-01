75651df9e3aadea10f2e701b1629efd8ca2b4d7e
Luka Silic <silicluka520@gmail.com>
2023-05-01 11:05:12 -0400

    Working through the note, at where it reads "Adding Sounds" near the bottom of the second page of the note

--- Monogame Lesson 4 - Time and Sound/Game1.cs
+++ Monogame Lesson 4 - Time and Sound/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading;
 
 namespace Monogame_Lesson_4___Time_and_Sound
 {
@@ -8,6 +9,11 @@ namespace Monogame_Lesson_4___Time_and_Sound
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        SpriteFont timeFont;
+        Rectangle bombRect;
+        Texture2D bombTexture;
+        MouseState mouseState;
+        float seconds, startTime;
 
         public Game1()
         {
@@ -18,7 +24,11 @@ namespace Monogame_Lesson_4___Time_and_Sound
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            Window.Title = "Bombs Away";
+            bombRect = new Rectangle(50, 50, 700, 400);
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -26,16 +36,26 @@ namespace Monogame_Lesson_4___Time_and_Sound
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            timeFont = Content.Load<SpriteFont>("Font");
+            bombTexture = Content.Load<Texture2D>("bomb");
+            _graphics.ApplyChanges();
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds; // finished here
+            }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            seconds = (float)gameTime.TotalGameTime.TotalSeconds - startTime;
+            if (seconds > 10)
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+
+            
 
             base.Update(gameTime);
         }
@@ -43,8 +63,12 @@ namespace Monogame_Lesson_4___Time_and_Sound
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+            _spriteBatch.DrawString(timeFont, (60.0 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }

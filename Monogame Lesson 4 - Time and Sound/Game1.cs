d72e12ae052508566c0d31e577787a73edcbaba6
Luka Silic <silicluka520@gmail.com>
2023-05-03 11:04:56 -0400

    Trying to find a way to place a click-hitbox on the screen and wires of the bomb, but I don't know how to find the coordinates or dimensions for the rectangles.

--- Monogame Lesson 4 - Time and Sound/Game1.cs
+++ Monogame Lesson 4 - Time and Sound/Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Threading;
@@ -10,10 +11,13 @@ namespace Monogame_Lesson_4___Time_and_Sound
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         SpriteFont timeFont;
-        Rectangle bombRect;
-        Texture2D bombTexture;
+        SoundEffect explodeSFX;
+        SoundEffectInstance explodeSFXInstance;
+        Rectangle bombRect, explosionRect, bombScreenRect, bombWiresRect;
+        Texture2D bombTexture, explosionTexture;
         MouseState mouseState;
         float seconds, startTime;
+        bool exploded = false;
 
         public Game1()
         {
@@ -28,6 +32,9 @@ namespace Monogame_Lesson_4___Time_and_Sound
             _graphics.PreferredBackBufferHeight = 500;
             Window.Title = "Bombs Away";
             bombRect = new Rectangle(50, 50, 700, 400);
+            explosionRect = new Rectangle(0, 0, 900, 700);
+            bombScreenRect = new Rectangle(50, 50, 400, 250);  // complete guess
+
             _graphics.ApplyChanges();
 
             base.Initialize();
@@ -38,6 +45,9 @@ namespace Monogame_Lesson_4___Time_and_Sound
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             timeFont = Content.Load<SpriteFont>("Font");
             bombTexture = Content.Load<Texture2D>("bomb");
+            explosionTexture = Content.Load<Texture2D>("explosionIMG");
+            explodeSFX = Content.Load<SoundEffect>("explosion");
+            explodeSFXInstance = explodeSFX.CreateInstance();
             _graphics.ApplyChanges();
         }
 
@@ -46,7 +56,7 @@ namespace Monogame_Lesson_4___Time_and_Sound
             mouseState = Mouse.GetState();
             if (mouseState.LeftButton == ButtonState.Pressed)
             {
-                startTime = (float)gameTime.TotalGameTime.TotalSeconds; // finished here
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds;
             }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
@@ -55,7 +65,13 @@ namespace Monogame_Lesson_4___Time_and_Sound
             if (seconds > 10)
                 startTime = (float)gameTime.TotalGameTime.TotalSeconds;
 
-            
+            if (seconds >= 60)
+            {
+                explodeSFXInstance.Play();
+                exploded = true;
+            }
+            if (exploded && explodeSFXInstance.State == SoundState.Stopped)
+                Exit();
 
             base.Update(gameTime);
         }
@@ -67,9 +83,12 @@ namespace Monogame_Lesson_4___Time_and_Sound
 
             _spriteBatch.Draw(bombTexture, bombRect, Color.White);
             _spriteBatch.DrawString(timeFont, (60.0 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            if (exploded == true)
+            {
+                _spriteBatch.Draw(explosionTexture, explosionRect, Color.White);
+            }
 
             _spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
